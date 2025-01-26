c22652265bb25f3c97700100629a6db22d4d1ea9
User <user@MEOWW.local>
2025-01-25 19:12:55 -0800

    cmake

---
+++ CMakeFiles/RoseHack.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RoseHack.dir/Interface.cpp.o"
+  "CMakeFiles/RoseHack.dir/Interface.cpp.o.d"
+  "CMakeFiles/RoseHack.dir/Output.cpp.o"
+  "CMakeFiles/RoseHack.dir/Output.cpp.o.d"
+  "CMakeFiles/RoseHack.dir/User.cpp.o"
+  "CMakeFiles/RoseHack.dir/User.cpp.o.d"
+  "CMakeFiles/RoseHack.dir/main.cpp.o"
+  "CMakeFiles/RoseHack.dir/main.cpp.o.d"
+  "CMakeFiles/RoseHack.dir/workout.cpp.o"
+  "CMakeFiles/RoseHack.dir/workout.cpp.o.d"
+  "RoseHack"
+  "RoseHack.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RoseHack.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
